a51658c2b76f59f4fb9f0aa1561a8f30841a5745
mudskipper13 <MariaJumpMan001@hotmail.com>
2023-10-14 12:03:38 +0700

    Bug: Missing fixes for DNS + dynpal.
    I forgot about doing the rest of the fixes ALL the time-
    I only got fixed the obj event one lmao

--- src/field_effect_helpers.c
+++ src/field_effect_helpers.c
@@ -123,7 +123,7 @@ void LoadSpecialReflectionPalette(struct Sprite *sprite)
 	}
 	reflectionPalette.data = gReflectionPaletteBuffer;
 	reflectionPalette.tag = GetSpritePaletteTagByPaletteNum(sprite->oam.paletteNum) + 0x1000;
-	LoadSpritePalette(&reflectionPalette);
+	LoadSpritePaletteDayNight(&reflectionPalette);
 	sprite->oam.paletteNum = IndexOfSpritePaletteTag(reflectionPalette.tag);
 	UpdatePaletteGammaType(sprite->oam.paletteNum, GAMMA_ALT);
 	UpdateSpritePaletteWithWeather(sprite->oam.paletteNum);
